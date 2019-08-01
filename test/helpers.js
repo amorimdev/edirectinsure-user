2d15848cf32e2c125d178489f225153c2c677b1b
Jhonatan Amorim <amorim.dev@gmail.com>
2019-08-01 13:14:28 -0300

    update seneca test helper

--- test/helpers.js
+++ test/helpers.js
@@ -1,19 +1,10 @@
 'use strict'
 
-function Seneca (done) {
-  return new Promise((resolve, reject) => {
-    const seneca = require('../srv/start')
-    seneca.test(done)
-    seneca.ready(err => {
-      if (err) return reject(err)
-      return resolve(seneca)
-    })
+module.exports.Seneca = done => new Promise((resolve, reject) => {
+  const seneca = require('../srv/start')
+  seneca.test(done)
+  seneca.ready(err => {
+    if (err) return reject(err)
+    return resolve(seneca)
   })
-}
-
-const LOG_TAG = 'LOG::[AUTH | TEST]'
-
-module.exports = {
-  Seneca,
-  LOG_TAG
-}
+})
